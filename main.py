941892e4caf355de500372342c545eeb31377569
Mayra Moyano <mayramoyano2511@gmail.com>
2025-06-05 18:11:43 -0300

    main

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-"Principal"
+
+class Calculadora():
+    def __init__(self):
+        pass
+    
+    print("Calculadora iniciada")
+    a = float(input("Ingrese el primer numero: "))
+    b = float(input("Ingrese el segundo numero: "))
+    opcion = input("Ingrese la operacion deseada (suma, resta, multiplicacion, division): ").strip().lower()
+    if opcion == "resta":
+        from resta import restar
+        resultado = restar(a,b)
+        print(resultado)
+    elif opcion == "division":
+        from division import dividir
+        resultado = dividir(a,b)
+        print(resultado)
+    else:
+        print("Operacion no soportada")
